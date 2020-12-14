45aaea2024ff736bcd8626f97828b85f894f7ae0
cllowreda <h>
2020-12-14 13:03:46 +0100

    misc

--- srv/lib/server/router.js
+++ srv/lib/server/router.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router();
 const bodyParser = require('body-parser');
 
-router.use(bodyParser.json()); 
+router.use(bodyParser.json());
 
 router.use(bodyParser.urlencoded({
   extended: true
