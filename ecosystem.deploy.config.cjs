f30d7b5c3f86064590d3cb77c2fd452d59251e47
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2022-09-10 17:48:12 +0200

    misc

--- ecosystem.deploy.config.cjs
+++ ecosystem.deploy.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
 			"pre-deploy-local": "git commit -a -m 'Commit deploy bundle src' ; git checkout deploy && git merge master ; npm run bundle ; mv dist public && git add . && git commit -m 'Commit deploy bundle' ; git push origin && git checkout master",
 
 			"pre-deploy": "cd /root/Source/Deploy && docker-compose down --remove-orphans",
-			"post-deploy": "cd /root/Source/Deploy && mv mnio/source/public mnio/source/dist && docker-compose up --build -d",
+			"post-deploy": "cd /root/Source/Deploy && mv mnio/source/public dist && docker-compose up --build -d",
 		}
 	}
 };
