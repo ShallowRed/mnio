11ee2750bcc01f446285c4740ebff4c2ba2f9295
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2022-09-10 16:11:13 +0200

    Deploying

--- ecosystem.deploy.config.cjs
+++ ecosystem.deploy.config.cjs
@@ -4,11 +4,8 @@ module.exports = {
 
 		production: {
 
-			"key": "~/.ssh/id_ed25519",
-			"user": "root",
-			"host": [{
-				"host": "roonie.duque.bzh",
-			}],
+			"host": "rooniax",
+
 			"ref": "origin/deploy",
 			"repo": "git@github.com:ShallowRed/mnio.git",
 			"path": "/root/Source/mnio",
