3f35f1be99a17269283dbfddb9dfd01360ae8063
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2022-09-11 08:21:34 +0200

    Deploy build

--- ecosystem.deploy.config.cjs
+++ ecosystem.deploy.config.cjs
@@ -1,3 +1,5 @@
+const REMOTE_PATH = "/root/Source/Deploy";
+
 module.exports = {
 
 	deploy: {
@@ -5,18 +7,18 @@ module.exports = {
 		production: {
 
 			"host": "mnio-deploy",
-			"path": `/root/Source/Deploy/mnio`,
+			"path": `${REMOTE_PATH}/mnio`,
 
 			"repo": "git@github.com:ShallowRed/mnio.git",
 			"ref": "origin/deploy",
 
-			'pre-setup': `rm -rf /root/Source/Deploy/mnio`,
+			'pre-setup': `rm -rf ${REMOTE_PATH}/mnio`,
 
 			"pre-deploy-local": "npm run bundle && mv dist public && npm run push-deploy && mv public dist && npm run push-master",
 
-			"pre-deploy": `rm -rf /root/Source/Deploy/mnio/source/dist ; cd /root/Source/Deploy && docker-compose down --remove-orphans`,
+			"pre-deploy": `rm -rf ${REMOTE_PATH}/mnio/source/dist ; cd ${REMOTE_PATH} && docker-compose down --remove-orphans`,
 
-			"post-deploy": `mv /root/Source/Deploy/mnio/source/public /root/Source/Deploy/mnio/source/dist ; cd /root/Source/Deploy && docker-compose up --build -d`,
+			"post-deploy": `mv ${REMOTE_PATH}/mnio/source/public ${REMOTE_PATH}/mnio/source/dist ; cd ${REMOTE_PATH} && docker-compose up --build -d`,
 		}
 	}
 };
