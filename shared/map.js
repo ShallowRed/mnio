08a1a01d2f243ff40bbc60184ec171813ddd2748
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2024-01-31 21:16:15 +0100

    let's wrap things up

--- shared/map.js
+++ shared/map.js
@@ -1,70 +1,52 @@
 export default class SharedGameMap {
 
-	constructor({ gridState, cols, rows }) {
+  constructor({ gridState, cols, rows }) {
+    this.gridState = gridState;
+    this.cols = cols;
+    this.rows = rows;
+  }
 
-		this.gridState = gridState;
-		
-		this.cols = cols;
-		
-		this.rows = rows;
-	}
+  indexToCoords(index) {
+    const x = index % this.cols;
+    const y = (index - x) / this.rows;
+    return [x, y];
+  }
 
-	indexToCoords(index) {
+  coordsToIndex([x, y]) {
+    return this.cols * y + x;
+  }
 
-		const x = index % this.cols;
+  checkMove(player, direction) {
 
-		const y = (index - x) / this.rows;
+    let [x, y] = this.indexToCoords(player.position);
 
-		return [x, y];
-	}
+    if (direction == "left" && x !== 0) {
+      x--;
+    } else if (direction == "right" && x !== this.cols - 1) {
+      x++;
+    } else if (direction == "up" && y !== 0) {
+      y--;
+    } else if (direction == "down" && y !== this.rows - 1) {
+      y++;
+    } else {
+      return;
+    }
 
-	coordsToIndex([x, y]) {
+    const targetPosition = this.coordsToIndex([x, y]);
+    if (this.isAvailable(targetPosition, player)) {
+      return targetPosition;
+    }
+  }
 
-		return this.cols * y + x;
-	}
+  isAvailable(position, player) {
 
-	checkMove(player, direction) {
-
-		let [x, y] = this.indexToCoords(player.position);
-
-		if (direction == "left" && x !== 0) {
-
-			x--;
-
-		} else if (direction == "right" && x !== this.cols - 1) {
-
-			x++;
-
-		} else if (direction == "up" && y !== 0) {
-
-			y--;
-
-		} else if (direction == "down" && y !== this.rows - 1) {
-
-			y++;
-
-		} else {
-
-			return;
-		}
-
-		const targetPosition = this.coordsToIndex([x, y]);
-
-		if (this.isAvailable(targetPosition, player)) {
-
-			return targetPosition;
-		}
-	}
-
-	isAvailable(position, player) {
-
-		return (
-			player.ownCells.includes(position) ||
-			(
-				player.allowedCells.includes(position) &&
-				!this.game.players.positions.includes(position) &&
-				!this.gridState[position]
-			)
-		);
-	}
+    return (
+      player.ownCells.includes(position) ||
+      (
+        player.allowedCells.includes(position) &&
+        !this.game.players.positions.includes(position) &&
+        !this.gridState[position]
+      )
+    );
+  }
 }
--- shared/players.js
+++ shared/players.js
@@ -5,27 +5,22 @@ export default class SharedPlayers {
 	constructor() {}
 
 	get(userId) {
-
 		return this.collection.get(userId);
 	}
 
 	set(player) {
-
 		this.collection.set(player.userId, player);
 	}
 
 	delete(userId) {
-
 		this.collection.delete(userId);
 	}
 
 	get values() {
-
 		return [...this.collection.values()];
 	}
 
 	get positions() {		
-
 		return this.values.map(player => player.position);
 	}
 }
